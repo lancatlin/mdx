f4705db99150c60d0f15754ce821eff488847798
Justin Lin <wancat@wancat.cc>
2025-04-04 22:47:36 +0800

    feat: read template file name from arg

--- src/mdx/main.py
+++ src/mdx/main.py
@@ -2,13 +2,13 @@ from .parser import Parser
 import sys
 
 def main() -> None:
-    filename = "ledger.md"
+    filename = sys.argv[1]
     with open(filename, 'r') as rfile:
         content = rfile.read()
 
     parser = Parser(content)
     doc = parser.parse()
-    doc.parse_arg(sys.argv[1:])
+    doc.parse_arg(sys.argv[2:])
     output = doc.execute()
     with open(doc.get_filename(), "w") as wfile:
         wfile.write(output)
